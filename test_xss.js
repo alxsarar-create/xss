93791652e93fd971ec386ed71bf6d8268ddf114b
alxsarar-create <alxsar.ar@gmail.com>
2026-05-28 15:36:17 -0300

    Update test_xss.js

--- test_xss.js
+++ test_xss.js
@@ -1,1 +1,21 @@
-alert(document.domain)
+
+if (document.location.hash) {
+   var frag = document.location.hash.match(/[^#][^#]*/)[0];
+}
+
+if (frag) {
+
+  try {
+
+    eval(frag);
+
+  } catch(e) {
+
+    alert("URL fragment (#) contains invalid JS code, try again!");
+  }
+
+} else {
+
+  alert(1);
+
+}
